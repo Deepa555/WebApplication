c10159236351cfc7262e4f6001e7c31973867d07
Deepa555 <71755530+Deepa555@users.noreply.github.com>
2021-05-12 00:18:07 +0530

    Update BuildandDeploy.ps1

--- BuildandDeploy.ps1
+++ BuildandDeploy.ps1
@@ -1,1 +1,1 @@
-﻿"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" WebApplication1.sln
+C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe WebApplication1.sln
