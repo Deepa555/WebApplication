0381291ee59c889360031d01e03c50639f61ddab
Deepa555 <71755530+Deepa555@users.noreply.github.com>
2021-05-12 00:23:16 +0530

    Update BuildandDeploy.ps1

--- BuildandDeploy.ps1
+++ BuildandDeploy.ps1
@@ -1,1 +1,2 @@
-C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe WebApplication1.sln
+$msbuild = '"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"'
+iex ("& {0} {1}" -f $msbuild, "-version")
