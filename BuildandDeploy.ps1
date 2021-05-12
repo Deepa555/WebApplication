505ea813ae0d055b9df341c4248030a9a32b4ecf
Deepa555 <71755530+Deepa555@users.noreply.github.com>
2021-05-12 11:34:19 +0530

    Update BuildandDeploy.ps1

--- BuildandDeploy.ps1
+++ BuildandDeploy.ps1
@@ -1,7 +1,5 @@
 $msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 Set-Alias MSBuildExe $msbuildpath
 Write-Host "Building the project" 
-$destpath="C:\Alarmnet1"
-New-Item $destpath -ItemType directory
-MSBuildExe WebApplication1.sln
+dotnet build WebApplication1.sln --configuration Release
 Write-Host "Building the Sucess" 
