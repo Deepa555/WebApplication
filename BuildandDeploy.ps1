bc63863655f616e24a1796cf3e0bceb1efbd8a6e
Deepa555 <71755530+Deepa555@users.noreply.github.com>
2021-05-12 11:21:53 +0530

    Update BuildandDeploy.ps1

--- BuildandDeploy.ps1
+++ BuildandDeploy.ps1
@@ -1,3 +1,5 @@
 $msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 Set-Alias MSBuildExe $msbuildpath
+Write-Host "Building the project" 
 MSBuildExe WebApplication1.sln
+Write-Host "Building the Sucess" 
