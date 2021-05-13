732aa99a07e8121161eecbe9e8a337e2f3e4626b
Deepa555 <71755530+Deepa555@users.noreply.github.com>
2021-05-13 11:28:41 +0530

    Update BuildandDeploy.ps1

--- BuildandDeploy.ps1
+++ BuildandDeploy.ps1
@@ -1,1 +1,7 @@
-dotnet build WebApplication1.sln --configuration Release
+$msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
+Set-Alias MSBuildExe $msbuildpath
+Write-Host "Building the project" 
+MSBuildExe WebApplication1.sln /p:Configuration=Release /p:DeployOnBuild=true /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=true /p:publishUrl=C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\publish\MySampleApplication\Testing
+Write-Host "Building the Sucess"
+
+
